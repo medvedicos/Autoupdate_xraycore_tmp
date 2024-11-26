b729e34dd517e72d0aff41d772eb70e00964b5bc
medvedicos <41338299+medvedicos@users.noreply.github.com>
2024-11-27 00:50:35 +0300

    Update owo.sh

--- owo.sh
+++ owo.sh
@@ -112,7 +112,7 @@ fi
 
 echo -e "${MAGENTA} INSTALLING XRAY ... ${ENDCOLOR}"
 opkg install panel.ipk -d ram
-cd /tmp/usr/bin/xray
+cd /tmp/usr/bin
 rm xray
 wget -q https://drive.usercontent.google.com/u/0/uc?id=1KQo-HVoWRKnH1Iyh59SKUimQ1vNNA549&export=download
 cd /tmp
