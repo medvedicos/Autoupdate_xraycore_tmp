732d931e97b7cb81dc77577c2b9737eac2feb9b1
medvedicos <41338299+medvedicos@users.noreply.github.com>
2024-12-21 22:45:55 +0300

    Update amirhossein.sh

--- amirhossein.sh
+++ amirhossein.sh
@@ -386,12 +386,7 @@ uci commit system
 
 cd
 
-uci set system.@system[0].hostname=By-AmirHossein
-
 uci commit system
-
-uci set dhcp.@dnsmasq[0].rebind_domain='www.ebanksepah.ir 
-my.irancell.ir'
 
 uci set passwall2.@global_app[0].xray_file='/tmp/usr/bin/xray' >/dev/null 2>&1
 
